7ab1b573cd4b525f7aed3c67d71349aae14c5579
Lyle Hart III <lyle.hart.3@gmail.com>
2016-03-05 21:54:56 -0500

    Ready for Code Review, added a DateTimeExtensions class and implemented it.

---
+++ BeginnerAgeProcessing/BeginnerAgeProcessing/DateTimeExtensions.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeginnerAgeProcessing
+{
+    static class DateTimeExtensions
+    {
+        public static int AgeInYears (DateTime age1, DateTime age2)
+        {
+            return Math.Abs(age1.Year - age2.Year);
+        }
+        public static int AgeInMonths (DateTime age1, DateTime age2)
+        {
+             return ((age1.Year - age2.Year) * 12) + (age1.Month - age2.Month);
+        }
+        public static int AgeInDays (DateTime age1, DateTime age2)
+        {
+            return Convert.ToInt32(((age1.Year - age2.Year) * 365.25) + (age1.DayOfYear - age2.DayOfYear));
+        }
+
+        public static int AgePartYears(DateTime age1, DateTime age2)
+        {
+            return DateTimeSpan.CompareDates(age1, age2).Years;
+        }
+
+        public static int AgePartMonths(DateTime age1, DateTime age2)
+        {
+            return DateTimeSpan.CompareDates(age1, age2).Months;
+        }
+
+        public static int AgePartDays(DateTime age1, DateTime age2)
+        {
+            return DateTimeSpan.CompareDates(age1, age2).Days;
+        }
+    }
+
+
+    /// <summary>
+    /// http://stackoverflow.com/a/9216404/1214743
+    /// http://stackoverflow.com/users/189950/kirk-woll
+    /// answered Feb 9 '12 at 18:14
+    /// </summary>
+    public struct DateTimeSpan
+    {
+        private readonly int years;
+        private readonly int months;
+        private readonly int days;
+        private readonly int hours;
+        private readonly int minutes;
+        private readonly int seconds;
+        private readonly int milliseconds;
+
+        public DateTimeSpan(int years, int months, int days, int hours, int minutes, int seconds, int milliseconds)
+        {
+            this.years = years;
+            this.months = months;
+            this.days = days;
+            this.hours = hours;
+            this.minutes = minutes;
+            this.seconds = seconds;
+            this.milliseconds = milliseconds;
+        }
+
+
+        public int Years { get { return years; } }
+        public int Months { get { return months; } }
+        public int Days { get { return days; } }
+        public int Hours { get { return hours; } }
+        public int Minutes { get { return minutes; } }
+        public int Seconds { get { return seconds; } }
+        public int Milliseconds { get { return milliseconds; } }
+
+        enum Phase { Years, Months, Days, Done }
+
+        public static DateTimeSpan CompareDates(DateTime date1, DateTime date2)
+        {
+            if (date2 < date1)
+            {
+                var sub = date1;
+                date1 = date2;
+                date2 = sub;
+            }
+
+            DateTime current = date1;
+            int years = 0;
+            int months = 0;
+            int days = 0;
+
+            Phase phase = Phase.Years;
+            DateTimeSpan span = new DateTimeSpan();
+
+            while (phase != Phase.Done)
+            {
+                switch (phase)
+                {
+                    case Phase.Years:
+                        if (current.AddYears(years + 1) > date2)
+                        {
+                            phase = Phase.Months;
+                            current = current.AddYears(years);
+                        }
+                        else
+                        {
+                            years++;
+                        }
+                        break;
+                    case Phase.Months:
+                        if (current.AddMonths(months + 1) > date2)
+                        {
+                            phase = Phase.Days;
+                            current = current.AddMonths(months);
+                        }
+                        else
+                        {
+                            months++;
+                        }
+                        break;
+                    case Phase.Days:
+                        if (current.AddDays(days + 1) > date2)
+                        {
+                            current = current.AddDays(days);
+                            var timespan = date2 - current;
+                            span = new DateTimeSpan(years, months, days, timespan.Hours, timespan.Minutes, timespan.Seconds, timespan.Milliseconds);
+                            phase = Phase.Done;
+                        }
+                        else
+                        {
+                            days++;
+                        }
+                        break;
+                }
+            }
+
+            return span;
+        }
+    }
+}
--- BeginnerAgeProcessing/BeginnerAgeProcessing/Program.cs
+++ BeginnerAgeProcessing/BeginnerAgeProcessing/Program.cs
@@ -15,7 +15,8 @@ namespace BeginnerAgeProcessing
         ///  and age difference between each consecutive sibling.
         ///  For example:
         ///  Please enter the number of siblings: 3
-        ///  Please enter date of birth of sibling 1: 01-01-1990 Please enter date of birth of sibling 2: 05-03-1995
+        ///  Please enter date of birth of sibling 1: 01-01-1990 
+        ///  Please enter date of birth of sibling 2: 05-03-1995
         ///  Please enter date of birth of sibling 3: 08-05-1998
         ///  Age of sibling 1 is: 25 years 2 months 19 days 
         ///  Age of sibling 2 is: 20 years 0 months 15 days 
@@ -28,14 +29,28 @@ namespace BeginnerAgeProcessing
             Console.WriteLine("Please enter the number of siblings");
             var siblingCount = new int();
             int.TryParse(Console.ReadLine(), out siblingCount);
-            var siblingDictionary = new Dictionary<int, Sibling>();
+            var siblings = new List<Sibling>();
+            //var siblingDictionary = new Dictionary<int, Sibling>();
             for (int i = 1; i < siblingCount + 1; i++)
             {
                 Console.Write("Please Enter the date of birth for sibling " + i.ToString() + ":");
-                var siblingDOB = DateTime.Parse(Console.ReadLine());
-                siblingDictionary.Add(i, new Sibling(siblingDOB));
-                Console.WriteLine(siblingDOB.ToString());                
+                siblings.Add(new Sibling(DateTime.Parse(Console.ReadLine())));
             }
+            siblings = (List<Sibling>)siblings.OrderByDescending(x => x.DOB).ToList<Sibling>();
+
+            for (int i = 1; i < siblings.Count + 1; i++)
+            {
+                var diff = DateTimeSpan.CompareDates(siblings[i - 1].DOB, DateTime.Now);
+                Console.WriteLine("Age of sibling " + i + " is: " + diff.Years + " years, " + diff.Months + " months and " + diff.Days + " days.");
+            }
+
+            for (var i = 1; i < siblingCount; i++)
+            {
+                var diff = DateTimeSpan.CompareDates(siblings[i - 1].DOB, siblings[i].DOB);
+                Console.WriteLine("Difference between sibling " + i.ToString() + " and " + (i + 1).ToString() + " is " +
+                    diff.Years + " years, " + diff.Months + " months and " + diff.Days + " days.");
+            }
+
             Console.ReadLine();
         }
     }
